362db48c42e1433e728d92e1f9ce36dd9c5a7a74
阿栋的仓库 <2675413003@qq.com>
2021-02-01 09:37:24 +0800

    first commit

---
+++ src/components/Tabs.vue
@@ -0,0 +1,70 @@
+<template>
+  <ul class="tabs" :class="{ [classPrefix + '-tabs']: classPrefix }">
+    <li
+      v-for="item in dataSource"
+      :key="item.value"
+      class="tabs-item"
+      @click="select(item)"
+      :class="liClass(item)"
+    >
+      {{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<script lang ='ts'>
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+type DataSourceItem = { text: string; value: string };
+
+@Component
+export default class Tabs extends Vue {
+  @Prop({ required: true, type: Array })
+  dataSource!: DataSourceItem[];
+  @Prop(String)
+  readonly value!: string;
+  @Prop(String)
+  classPrefix?: string;
+
+  liClass(item: DataSourceItem) {
+    return {
+      [this.classPrefix + "-tabs-item"]: this.classPrefix,
+      selected: item.value === this.value,
+    };
+  }
+
+  select(item: DataSourceItem) {
+    this.$emit("update:value", item.value);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.tabs {
+  background: #ffda47;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+
+  &-item {
+    width: 25%;
+    height: 64px;
+    display: flex;
+    margin: 0 auto;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
